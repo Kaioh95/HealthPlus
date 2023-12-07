f1dcc5df6267d358fac4918f1626d381ba64e729
KaioHenrique <kaioh95@gmail.com>
2023-12-07 16:21:09 -0300

    Refund jml annotations

--- src/main/java/Cashier/Refund.java
+++ src/main/java/Cashier/Refund.java
@@ -7,19 +7,28 @@ import javafx.scene.image.ImageView;
  * @author heshan
  */
 public class Refund {
-    private final SimpleStringProperty patientID = new SimpleStringProperty("");
-    private final SimpleStringProperty date = new SimpleStringProperty("");
-    private final SimpleStringProperty billID = new SimpleStringProperty("");
-    private final SimpleStringProperty service = new SimpleStringProperty("");
-    private final SimpleStringProperty bill = new SimpleStringProperty("");
-    private ImageView refund;
-    
+    /*@ nullable spec_public @*/ private final SimpleStringProperty patientID = new SimpleStringProperty("");
+    /*@ nullable spec_public @*/ private final SimpleStringProperty date = new SimpleStringProperty("");
+    /*@ nullable spec_public @*/ private final SimpleStringProperty billID = new SimpleStringProperty("");
+    /*@ nullable spec_public @*/ private final SimpleStringProperty service = new SimpleStringProperty("");
+    /*@ nullable spec_public@*/ private final SimpleStringProperty bill = new SimpleStringProperty("");
+    /*@ nullable spec_public @*/ private ImageView refund;
+
+    //@ public invariant patientID != null && date != null && billID != null && service != null && bill != null && refund != null;
+
     /*
     public Refund() {
         this("", "","","","","");
     }
     */
-    
+
+    //@ requires patientID != null && date != null && billID != null && service != null && bill != null && refund != null;
+    //@ ensures getPatientID().equals(patientID);
+    //@ ensures getDate().equals(date);
+    //@ ensures getBillID().equals(billID);
+    //@ ensures getService().equals(service);
+    //@ ensures getBill().equals(bill);
+    //@ ensures getRefund().equals(refund);
     public Refund(String patientID,String date, String billID, String service, String bill,  ImageView refund) {
         setPatientID(patientID);
         setDate(date);
@@ -29,50 +38,74 @@ public class Refund {
         setRefund(refund);
     }
 
+    //@ ensures \result != null;
+    //@ pure
     public String getPatientID() {
         return patientID.get();
     }
- 
+
+    //@ requires value != null;
+    //@ ensures getPatientID().equals(value);
     public void setPatientID(String value) {
         patientID.set(value);
     }
-    
+
+    //@ ensures \result != null;
+    //@ pure
     public String getDate() {
         return date.get();
     }
- 
+
+    //@ requires value != null;
+    //@ ensures getDate().equals(value);
     public void setDate(String value) {
         date.set(value);
     }
-    
+
+    //@ ensures \result != null;
+    //@ pure
     public String getBillID() {
         return billID.get();
     }
-    
+
+    //@ requires value != null;
+    //@ ensures getBillID().equals(value);
     public void setBillID(String value) {
         billID.set(value);
     }
-    
+
+    //@ ensures \result != null;
+    //@ pure
     public String getService() {
         return service.get();
     }
-    
+
+    //@ requires value != null;
+    //@ ensures getService().equals(value);
     public void setService(String value) {
         service.set(value);
     }
-    
+
+    //@ ensures \result != null;
+    //@ pure
     public String getBill() {
         return bill.get();
     }
-    
+
+    //@ requires value != null;
+    //@ ensures getBill().equals(value);
     public void setBill(String value) {
         bill.set(value);
     }
-    
+
+    //@ ensures \result != null;
+    //@ pure
     public ImageView getRefund() {
         return this.refund;
     }
-    
+
+    //@ requires value != null;
+    //@ ensures getRefund().equals(value);
     public void setRefund(ImageView value) {
         refund = value;
     }
