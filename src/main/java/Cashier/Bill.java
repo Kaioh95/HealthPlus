4a7952954e901bfff64274e34fb5b46692643da5
giooogk <macedo1999.gk@gmail.com>
2023-12-07 23:20:31 -0300

    Bill jml annotations

--- src/main/java/Cashier/Bill.java
+++ src/main/java/Cashier/Bill.java
@@ -6,108 +6,158 @@ import javafx.beans.property.SimpleStringProperty;
  * @author heshan
  */
 public class Bill {
+    //@ spec_public
     private final SimpleStringProperty patientID = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty date = new SimpleStringProperty("");
-    
+
+    //@ spec_public
     private final SimpleStringProperty doctor = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty hospital = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty pharmacy = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty laboratory = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty appointment = new SimpleStringProperty("");
-    
+
+    //@ spec_public
     private final SimpleStringProperty bill = new SimpleStringProperty("");
+
+    //@ spec_public
     private final SimpleStringProperty billID = new SimpleStringProperty("");
-    
-    
-    
+
+    //@ public invariant patientID != null;
+    //@ public invariant date != null;
+    //@ public invariant doctor != null;
+    //@ public invariant hospital != null;
+    //@ public invariant pharmacy != null;
+    //@ public invariant laboratory != null;
+    //@ public invariant appointment != null;
+    //@ public invariant bill != null;
+    //@ public invariant billID != null;
+
+    //@ ensures getPatientID() != null && getDate() != null && getDoctor() != null && getHospital() != null && getPharmacy() != null && getLaboratory() != null && getAppointment() != null && getBill() != null && getBillID() != null;
     public Bill() {
-        this("", "", "","","","","","","");
+        this("", "", "", "", "", "", "", "", "");
     }
- 
-    public Bill(String patientID, String date, String doctor, String hospital, String pharmacy, String laboratory, String appointment,String bill,String billID) 
-    {
+
+    //@ requires patientID != null && date != null && doctor != null && hospital != null && pharmacy != null && laboratory != null && appointment != null && bill != null && billID != null;
+    //@ ensures getPatientID().equals(patientID) && getDate().equals(date) && getDoctor().equals(doctor) && getHospital().equals(hospital) && getPharmacy().equals(pharmacy) && getLaboratory().equals(laboratory) && getAppointment().equals(appointment) && getBill().equals(bill) && getBillID().equals(billID);
+    public Bill(String patientID, String date, String doctor, String hospital, String pharmacy, String laboratory, String appointment, String bill, String billID) {
         setPatientID(patientID);
         setDate(date);
-        
         setDoctor(doctor);
         setHospital(hospital);
         setPharmacy(pharmacy);
         setLaboratory(laboratory);
         setAppointment(appointment);
-        
         setBill(bill);
         setBillID(billID);
     }
 
-    public String getPatientID() {
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getPatientID() {
         return patientID.get();
     }
- 
-    public void setPatientID(String value) {
+
+    //@ requires value != null;
+    //@ ensures getPatientID().equals(value);
+    public void setPatientID(/*@ non_null @*/ String value) {
         patientID.set(value);
     }
-        
-    public String getDate() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getDate() {
         return date.get();
     }
-    
-    public void setDate(String value) {
+
+    //@ requires value != null;
+    //@ ensures getDate().equals(value);
+    public void setDate(/*@ non_null @*/ String value) {
         date.set(value);
     }
-    
-    public String getDoctor() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getDoctor() {
         return doctor.get();
     }
-    
-    public void setDoctor(String value) {
+
+    //@ requires value != null;
+    //@ ensures getDoctor().equals(value);
+    public void setDoctor(/*@ non_null @*/ String value) {
         doctor.set(value);
     }
-    
-    public String getHospital() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getHospital() {
         return hospital.get();
     }
-    
-    public void setHospital(String value) {
+
+    //@ requires value != null;
+    //@ ensures getHospital().equals(value);
+    public void setHospital(/*@ non_null @*/ String value) {
         hospital.set(value);
     }
-    
-    public String getPharmacy() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getPharmacy() {
         return pharmacy.get();
     }
-    
-    public void setPharmacy(String value) {
+
+    //@ requires value != null;
+    //@ ensures getPharmacy().equals(value);
+    public void setPharmacy(/*@ non_null @*/ String value) {
         pharmacy.set(value);
     }
-    
-    public String getLaboratory() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getLaboratory() {
         return laboratory.get();
     }
-    
-    public void setLaboratory(String value) {
+
+    //@ requires value != null;
+    //@ ensures getLaboratory().equals(value);
+    public void setLaboratory(/*@ non_null @*/ String value) {
         laboratory.set(value);
     }
-    
-    public String getAppointment() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getAppointment() {
         return appointment.get();
     }
-    
-    public void setAppointment(String value) {
+
+    //@ requires value != null;
+    //@ ensures getAppointment().equals(value);
+    public void setAppointment(/*@ non_null @*/ String value) {
         appointment.set(value);
     }
-    
-    public String getBill() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getBill() {
         return bill.get();
     }
-    
-    public void setBill(String value) {
+
+    //@ requires value != null;
+    //@ ensures getBill().equals(value);
+    public void setBill(/*@ non_null @*/ String value) {
         bill.set(value);
     }
-    
-    public String getBillID() {
+
+    //@ ensures \result != null;
+    public /*@ pure @*/ String getBillID() {
         return billID.get();
     }
-    
-    public void setBillID(String value) {
+
+    //@ requires value != null;
+    //@ ensures getBillID().equals(value);
+    public void setBillID(/*@ non_null @*/ String value) {
         billID.set(value);
     }
 }
