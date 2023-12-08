1df1b001abe6bb9726b5201be432d9ee9b3e83fe
giooogk <macedo1999.gk@gmail.com>
2023-12-07 23:19:27 -0300

    Drug jml annotations

--- src/main/java/Pharmacist/Drug.java
+++ src/main/java/Pharmacist/Drug.java
@@ -6,17 +6,28 @@ import javafx.beans.property.SimpleStringProperty;
  * @author heshan
  */
 public class Drug{
-    private final SimpleStringProperty name = new SimpleStringProperty("");
-    private final SimpleStringProperty type = new SimpleStringProperty("");
-    private final SimpleStringProperty unit = new SimpleStringProperty("");
-    private final SimpleStringProperty price = new SimpleStringProperty("");
-    final SimpleStringProperty amount = new SimpleStringProperty("");
-    private final SimpleStringProperty suppliers = new SimpleStringProperty("");
-    
+    //@ invariant name != null && type != null && unit != null && price != null && amount != null && suppliers != null;
+
+    //@ spec_public
+    final //@ non_null
+            SimpleStringProperty name = new SimpleStringProperty("");
+    final //@ non_null
+    SimpleStringProperty type = new SimpleStringProperty("");
+    final //@ non_null
+    SimpleStringProperty unit = new SimpleStringProperty("");
+    final //@ non_null
+    SimpleStringProperty price = new SimpleStringProperty("");
+    final //@ non_null
+    SimpleStringProperty amount = new SimpleStringProperty("");
+    final //@ non_null
+    SimpleStringProperty suppliers = new SimpleStringProperty("");
+
+    //@ ensures getName() != null && getType() != null && getUnit() != null && getPrice() != null && getAmount() >= 0 && getSuppliers() != null;
     public Drug() {
         this("", "", "","","","");
     }
- 
+
+    //@ ensures getName().equals(drugName) && getType().equals(drugType) && getUnit().equals(drugUnit) && getPrice().equals(unitPrice) && getAmount() == Integer.parseInt(amount) && getSuppliers().equals(suppliers);
     public Drug(String drugName, String drugType, String drugUnit, String unitPrice, String amount, String suppliers) {
         setName(drugName);
         setType(drugType);
@@ -26,51 +37,71 @@ public class Drug{
         setSuppliers(suppliers);
     }
 
-    public String getName() {
+    //@ ensures \result != null;
+    public /*@ non_null @*/ String getName() {
         return name.get();
     }
- 
-    public void setName(String value) {
+
+    /*@ requires value != null;
+      @ ensures getName().equals(value);
+      @ assignable name;
+      @*/
+    public void setName(/*@ non_null @*/ String value) {
         name.set(value);
     }
-        
-    public String getType() {
+
+    //@ ensures \result != null;
+    public /*@ non_null @*/ String getType() {
         return type.get();
     }
-    
-    public void setType(String value) {
+
+    //@ requires value != null;
+    //@ ensures getType().equals(value);
+    public void setType(/*@ non_null @*/ String value) {
         type.set(value);
     }
-    
-    public String getUnit() {
+
+    //@ ensures \result != null;
+    public /*@ non_null @*/ String getUnit() {
         return unit.get();
     }
-    
-    public void setUnit(String value) {
+
+    //@ requires value != null;
+    //@ ensures getUnit().equals(value);
+    public void setUnit(/*@ non_null @*/ String value) {
         unit.set(value);
     }
-    
-    public String getPrice() {
+
+    //@ ensures \result != null;
+    public /*@ non_null @*/ String getPrice() {
         return price.get();
     }
-    
-    public void setPrice(String value) {
+
+    //@ requires value != null;
+    //@ ensures getPrice().equals(value);
+    public void setPrice(/*@ non_null @*/ String value) {
         price.set(value);
     }
-    
+
+    //@ ensures \result >= 0;
     public int getAmount() {
         return Integer.parseInt(amount.get());
     }
-    
-    public void setAmount(String value) {
+
+    //@ requires value != null;
+    //@ ensures getAmount() == Integer.parseInt(value);
+    public void setAmount(/*@ non_null @*/ String value) {
         amount.set(value);
     }
-    
-    public String getSuppliers() {
+
+    //@ ensures \result != null;
+    public /*@ non_null @*/ String getSuppliers() {
         return suppliers.get();
     }
-    
-    public void setSuppliers(String value) {
+
+    //@ requires value != null;
+    //@ ensures getSuppliers().equals(value);
+    public void setSuppliers(/*@ non_null @*/ String value) {
         suppliers.set(value);
     }
     
